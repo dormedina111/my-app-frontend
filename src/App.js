3f42d58d99a9acc10028a64340e07bdd71122d79
dormedina111 <110923343+dormedina111@users.noreply.github.com>
2024-05-23 13:38:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Lobby from './components/Lobby';
 import CodeBlockPage from './components/CodeBlockPage';
 
 function App() {
+    const [codeBlocks, setCodeBlocks] = useState([]);
+
+    useEffect(() => {
+        // Fetch code-blocks from the server
+        fetch('http://localhost:5000/api/code-blocks')
+            .then(response => response.json())
+            .then(data => setCodeBlocks(data))
+            .catch(error => console.error('Error fetching code blocks:', error));
+    }, []);
+
     return (
       <Router>
             <Routes>
-                <Route path="/" element={<Lobby />} />
-                <Route path="/codeblock/:id" element={<CodeBlockPage />} />
+                <Route path="/" element={<Lobby codeBlocks={codeBlocks} />} />
+                <Route path="/codeblock/:id" element={<CodeBlockPage codeBlocks={codeBlocks} />} />
             </Routes>
         </Router>
     );
